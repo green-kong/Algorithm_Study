6e0334867df46bebc43a605e43eadd35f5d913ab
easy-young <leejy230698@naver.com>
2022-03-14 22:36:16 +0900

    BJ_1931_jy.js UPDATE

--- 2week/BJ_1931/BJ_1931_jy.js
+++ 2week/BJ_1931/BJ_1931_jy.js
@@ -8,22 +8,33 @@ for (let i = 1; i < input.length; i++) {
     arr[i-1][0] = parseInt(arr[i-1][0]);
     arr[i-1][1] = parseInt(arr[i-1][1]);
 }
-arr.sort((a, b)=> a[1] - b[1]);
+arr.sort((a, b)=> a[0] - b[0]).sort((a, b)=> a[1] - b[1]);
 
 let a = 0;
 let b = 1;
 
-for (let j = 0; j < arr.length-1; j++) {
-    while (1) {
-        if (b == arr.length) break;
-        if (arr[b][0] >= arr[a][1]) {
-            a = b;
-            answer++;
-            break;
-        }
+while (b < arr.length) {
+    if (arr[b][0] >= arr[a][1]) {
+        a = b;
         b++;
+        answer++;
+        continue;
     }
+    b++;
 }
+
+// for (let j = 0; j < arr.length-1; j++) {
+//     while (1) {
+//         if (b == arr.length) break;
+//         if (arr[b][0] >= arr[a][1]) {
+//             a = b;
+//             b++;
+//             answer++;
+//             break;
+//         }
+//         b++;
+//     }
+// }
 
 console.log(arr);
 console.log(answer);
